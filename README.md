74e01cc65ffb35308071c09ce295c30066a5f190
xy654123 <96267331+xy654123@users.noreply.github.com>
2022-06-06 21:01:21 +0900

    1.0 Initialization

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Nwitter
+
+Twitter (mini)clone with React and Firebase
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/components/Router.js
@@ -0,0 +1,33 @@
+import { HashRouter as Router, /*Redirect,*/ Route, Switch } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
+
+const AppRouter = ({ isLoggedIn, userObj }) => {
+    
+    return (
+        <Router>
+            {isLoggedIn && <Navigation />}
+            <Switch>
+                {isLoggedIn ? (
+                <>
+                    <Route exact path="/">
+                        <Home userObj={userObj}/>
+                    </Route>
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
+                </>
+                ) : (
+                    <Route exact path="/">
+                        <Auth />
+                    </Route>
+                )}
+                {/* <Redirect from="*" to="/" /> */}
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRouter;
